3d8de7005c01b2e52a1b7f7db14414c60234dac7
Emily Lu <emily.lu@outlook.com>
2022-02-26 13:14:57 +1100

    features

--- popup.js
+++ popup.js
@@ -1,15 +1,11 @@
 document.addEventListener('DOMContentLoaded', function() {
   document.getElementById("social_button").addEventListener("click", social_handler);
-});
-document.addEventListener('DOMContentLoaded', function() {
   document.getElementById("health_button").addEventListener("click", health_handler);
-});
-document.addEventListener('DOMContentLoaded', function() {
   document.getElementById("notes_button").addEventListener("click", notes_handler);
-});
-document.addEventListener('DOMContentLoaded', function() {
   document.getElementById("focus_button").addEventListener("click", focus_handler);
+  document.getElementById("meetings_button").addEventListener("click", meetings_handler);
 });
+
 
 // The handler also must go in a .js file
 function social_handler() {
@@ -21,6 +17,7 @@ function social_handler() {
       document.getElementById("health").style.display = 'none';
       document.getElementById("notes").style.display = 'none';
       document.getElementById("focus").style.display = 'none';
+      document.getElementById("meetings").style.display = 'none';  
   }
 }
 
@@ -32,7 +29,9 @@ function health_handler() {
     document.getElementById("social").style.display = 'none';
     document.getElementById("health").style.display = 'block';
     document.getElementById("notes").style.display = 'none';
-    document.getElementById("focus").style.display = 'none';  }
+    document.getElementById("focus").style.display = 'none';  
+    document.getElementById("meetings").style.display = 'none';  
+  }
 }
 function notes_handler() {
   if (document.getElementById("notes").style.display !== 'none') {
@@ -42,7 +41,9 @@ function notes_handler() {
     document.getElementById("social").style.display = 'none';
     document.getElementById("health").style.display = 'none';
     document.getElementById("notes").style.display = 'block';
-    document.getElementById("focus").style.display = 'none';  }
+    document.getElementById("focus").style.display = 'none';
+    document.getElementById("meetings").style.display = 'none';  
+  }
 }
 function focus_handler() {
   if (document.getElementById("focus").style.display !== 'none') {
@@ -52,5 +53,20 @@ function focus_handler() {
     document.getElementById("social").style.display = 'none';
     document.getElementById("health").style.display = 'none';
     document.getElementById("notes").style.display = 'none';
-    document.getElementById("focus").style.display = 'block';  }
+    document.getElementById("focus").style.display = 'block';
+    document.getElementById("meetings").style.display = 'none';  
+  }
+}
+
+function meetings_handler() {
+  if (document.getElementById("meetings").style.display !== 'none') {
+    document.getElementById("meetings").style.display = 'none';
+  }
+  else {
+    document.getElementById("social").style.display = 'none';
+      document.getElementById("health").style.display = 'none';
+      document.getElementById("notes").style.display = 'none';
+      document.getElementById("focus").style.display = 'none';
+      document.getElementById("meetings").style.display = 'block';
+  }
 }
