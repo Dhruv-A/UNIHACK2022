35d23bb74aae15d2719bd6fec76a13e325cd309a
Emily Lu <emily.lu@outlook.com>
2022-02-26 18:24:54 +1100

    removed repetitive code

--- popup.js
+++ popup.js
@@ -2,87 +2,38 @@ var curr_selection = null;
 
 document.addEventListener('DOMContentLoaded', function() {
   document.getElementById("social_button").addEventListener("click", function() {
-    const social_tool = document.getElementById("social");
-    handler(curr_selection, social_tool);
-    
+    curr_selection = handler(curr_selection, document.getElementById("social"));
+  });
+
+  document.getElementById("health_button").addEventListener("click", function() {
+    curr_selection = handler(curr_selection, document.getElementById("health"));
+  });
+
+  document.getElementById("notes_button").addEventListener("click", function() {
+    curr_selection = handler(curr_selection, document.getElementById("notes"));
+  });
+
+  document.getElementById("focus_button").addEventListener("click", function() {
+    curr_selection = handler(curr_selection, document.getElementById("focus"));
   });
 });
-document.addEventListener('DOMContentLoaded', function() {
-  document.getElementById("health_button").addEventListener("click", health_handler);
-});
-document.addEventListener('DOMContentLoaded', function() {
-  document.getElementById("notes_button").addEventListener("click", notes_handler);
-});
-document.addEventListener('DOMContentLoaded', function() {
-  document.getElementById("focus_button").addEventListener("click", focus_handler);
-});
+
 
 function handler(curr_selection, tool_select) {
-  console.log("click")
   if (curr_selection === tool_select) {
     tool_select.style.display = 'none';
-    curr_selection = null;
-    console.log("ping")
-  } else {
-    console.log("pong")
-    curr_selection = tool_select;
-    const tools = Array.from(
-      document.getElementsByClassName('tool')
-    );
-    tools.forEach(tool => {
-      if (tool === curr_selection) {
-        tool.style.display = 'block';
-      } else {
-        tool.style.display = 'none';
-      };
-    });
+    return null;
   }
-  
-  
-  
+
+  const tools = Array.from(document.getElementsByClassName('tool'));
+  tools.forEach(tool => {
+    tool.style.display = ((tool !== tool_select) ? 'none' : 'block');
+  });
+
+  // const tool_icons = Array.from(document.getElementsByClassName('home-btn'));
+  // tool_icons.forEach(icon => {
+  //   icon.style.color = ((icon.id !== tool_select) ? 'red' : 'pink');
+  // });
+
+  return tool_select
 }
-
-// The handler also must go in a .js file
-// function social_handler(selected) {
-//   selected = getElementById("social")
-//   if (document.getElementById("social").style.display !== 'none') {
-//     document.getElementById("social").style.display = 'none';
-//   }
-//   else {
-//     document.getElementById("social").style.display = 'block';
-//     document.getElementById("health").style.display = 'none';
-//     document.getElementById("notes").style.display = 'none';
-//     document.getElementById("focus").style.display = 'none';
-//   }
-// }
-
-// function health_handler() {
-//   if (document.getElementById("health").style.display !== 'none') {
-//     document.getElementById("health").style.display = 'none';
-//   }
-//   else {
-//     document.getElementById("social").style.display = 'none';
-//     document.getElementById("health").style.display = 'block';
-//     document.getElementById("notes").style.display = 'none';
-//     document.getElementById("focus").style.display = 'none';  }
-// }
-// function notes_handler() {
-//   if (document.getElementById("notes").style.display !== 'none') {
-//     document.getElementById("notes").style.display = 'none';
-//   }
-//   else {
-//     document.getElementById("social").style.display = 'none';
-//     document.getElementById("health").style.display = 'none';
-//     document.getElementById("notes").style.display = 'block';
-//     document.getElementById("focus").style.display = 'none';  }
-// }
-// function focus_handler() {
-//   if (document.getElementById("focus").style.display !== 'none') {
-//     document.getElementById("focus").style.display = 'none';
-//   }
-//   else {
-//     document.getElementById("social").style.display = 'none';
-//     document.getElementById("health").style.display = 'none';
-//     document.getElementById("notes").style.display = 'none';
-//     document.getElementById("focus").style.display = 'block';  }
-// }
